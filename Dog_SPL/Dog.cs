a069ffee22cb1b5e26b14258c889faac5d0b189a
sirsplove <sirsplove@gmail.com>
2020-01-30 15:40:58 -0500

    Initial check-in of module Dog_SPL

---
+++ Dog_SPL/Dog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dog_SPL
+{
+    public class Dog
+    {
+        private String name;
+        private int age;
+
+        public Dog()
+        {
+            age = 0;
+            name = "";
+        }
+
+        public Dog(int age, String name)
+        {
+            this.setAge(age);
+            this.setName(name);
+        }
+
+        public int getDogYears()
+        {
+            int dogYears = 15 + (this.age - 1) * 7;
+            return dogYears;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+
+        public void setAge(int age)
+        {
+            if (age < 0)
+                this.age = 0;
+            else
+                this.age = age;
+        }
+
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            bool valid = true;
+            char[] n = name.ToCharArray();
+
+            foreach (char c in n)
+            {
+                if (!Char.IsLetter(c))
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+                this.name = name;
+            else
+                this.name = "";
+        }
+
+        public override String ToString()
+        {
+            return "Dog [age=" + age + ", name=" + name + "]";
+        }
+
+    }
+}
---
+++ Dog_SPL/Dog_SPL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dog_SPL
+{
+    class DogRun
+    {
+
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.setName("Zeus");
+            dog.setAge(4);
+
+            Console.WriteLine("The dogs name is " + dog.getName());
+            Console.WriteLine(dog.getName() + " is " + dog.getAge() + " human years old.");
+            Console.WriteLine(dog.getName() + " is " + dog.getDogYears() + " dog years old.");
+            Console.WriteLine("The Dogs information: " + dog.ToString());
+
+        }
+    }
+}
